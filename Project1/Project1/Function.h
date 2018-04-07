c2b1837d8a242559fe87be261697bfc5214592a6
Jeffronis <35819347+Jeffronis@users.noreply.github.com>
2018-04-06 21:05:52 -0700

    Add files via upload

--- Project1/Project1/Function.h
+++ Project1/Project1/Function.h
@@ -11,17 +11,132 @@ char menu()
 		cout << "Choose one from the options below." << endl << endl;
 		cout << "1 - Play " << endl;
 		cout << "2 - next " << endl;
-		cout << "3 - Quit" << endl;
+		cout << "3 - back" << endl;
+		cout << "4 - stop music" << endl;
+		cout << "5 - add song" << endl;
+		cout << "6 - Quit" << endl;
 		cin >> userChoice;
 		cin.ignore();
 
-		if (userChoice != '1' && userChoice != '2' && userChoice != '3') {
+		if (userChoice != '1' && userChoice != '2' && userChoice != '3' &&  userChoice != '4' &&  userChoice != '5' &&  userChoice != '6') {
 			cout << endl;
 			cout << "Worng entry try again" << endl;
 			system("pause");
 		}
 
-	} while (userChoice != '1' && userChoice != '2' && userChoice != '3');
+	} while (userChoice != '1' && userChoice != '2' && userChoice != '3' &&  userChoice != '4' &&  userChoice != '5' &&  userChoice != '6');
 
 	return userChoice;
+}
+
+void SongName(string Songname)
+{
+	cout << endl;
+	cout << Songname << endl;
+	
+}
+
+
+ class Playlist
+{
+public:
+
+	Playlist();
+	~Playlist();
+
+	string getName() 
+	{
+		return name;
+	}
+	
+
+	string GetMuisc(int l)
+	{
+		return playlistMusic[l];
+	}
+
+	void addNewSong(string fileName)
+	{
+		playlistMusic.push_back(fileName);
+
+		calculateSize();
+	}
+
+	int listSize()
+	{
+		return size;
+	}
+
+	//Work in porogress
+	void NextSong()
+	{
+		listLocation++;
+		if (listLocation + 1 > size)
+		{
+			listLocation--;
+			cout << "No more songs in List" << endl;
+		}
+		else
+		{
+
+		}
+	}
+	
+	void PreviousSong()
+	{
+
+	}
+
+private:
+	int size;
+	int listLocation;
+	string name;
+	vector<string> playlistMusic;
+
+	void calculateSize()
+	{
+		size = playlistMusic.size();
+	}
+};
+
+Playlist::Playlist()
+{
+	//load all music to list,(temporary better solution to load music later)
+	playlistMusic.push_back("Dejo_Bon_-_Wheelpower_Go_.wav");
+	playlistMusic.push_back("14_Rain_Spyro_the_Dragon_.wav");
+	listLocation = 0;
+	calculateSize();
+}
+
+Playlist::~Playlist()
+{
+}
+
+class PlayMusic
+{
+public:
+
+	void Play(string filename)
+	{
+		PlaySound(TEXT(filename.c_str()), NULL, SND_ASYNC);
+	}
+
+	void StopMusic()
+	{
+		PlaySound(NULL, 0, 0);
+	}
+
+	PlayMusic();
+	~PlayMusic();
+
+private:
+
+};
+
+PlayMusic::PlayMusic()
+{
+}
+
+PlayMusic::~PlayMusic()
+{
 }
--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,13 +1,16 @@
 /*
 Music Player\visulizer
-Team: Jeff, Garret
+Team: Jeff, Garrett
 */
 #include<iostream>
+#include<string>
 #include<Windows.h>
+#include<list>
+#include<vector>
 #include<mmsystem.h>
 #include<conio.h>
 
-#include"Function.h";
+#include"Function.h"
 
 using namespace std;
 
@@ -15,28 +18,92 @@ int main()
 {
 	char choice;
 	string userInput;
-	//menu
+	
+	string file;
+	string songName;
+	vector<string> Music;
+	string userSong;
+	
+	Playlist newPlayList;
+	PlayMusic  player;
+	int i = 0;
+
+	//Music = newPlayList.getMusicList();
 	do
-	{
+	{	//menu
+	
 		choice = menu();
 		switch (choice)
 		{
 		case '1':
+			
+			
+			file = newPlayList.GetMuisc(i);
+			//function to  display current song.
+			SongName(newPlayList.GetMuisc(i));
 			//This code will play .wav files
-			PlaySound(TEXT("Dejo_Bon_-_Wheelpower_Go_.wav"), NULL, SND_SYNC);
+			//PlaySound(TEXT(file.c_str()), NULL, SND_ASYNC);
+			player.Play(newPlayList.GetMuisc(i));
 			break;
+
 		case'2':
 			//next
-			//not impllimented yet
+			PlaySound(NULL, 0, 0);
+			i++;
+			if (i + 1 > newPlayList.listSize())
+			{
+				i--;
+				cout << "No more songs in list." << endl;
+			}
+			else 
+			{
+				player.Play(newPlayList.GetMuisc(i));
+				SongName(newPlayList.GetMuisc(i));
+				
+			}
 			break;
+			
 		case'3':
+			//bug case 3 runs imideatly after case two is selected
+			//back
+			PlaySound(NULL, 0, 0);
+			i--;
+			if (i < 0)
+			{
+				i++;
+				cout << "No more songs in list." << endl;
+			}
+			else
+			{
+				player.Play(newPlayList.GetMuisc(i));
+				SongName(newPlayList.GetMuisc(i));
+			}
+			
+			break;
+
+		case'4':
+			//stops sound
+			PlaySound(NULL, 0, 0);
 			//quit
 			break;
-		default:
+
+		case'5':
+			
+			//add song
+			cout << "Enter file name of song: ";
+			getline(cin, userSong);
+			cin.ignore();
+			newPlayList.addNewSong(userSong);
+
+			break;
+
+		case'6':
+			//quit program
+
 			break;
 		}
 
-	} while (choice);
+	} while (choice != '6');
 
 	
 	return 0;
